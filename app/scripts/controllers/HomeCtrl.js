6a3aec6575b290775d7b0c8be18e80610b4bbb12
Meir Taffel <artgardening@gmail.com>
2017-02-05 19:01:36 +0200

    start to get messages to display

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         this.rooms = Room.all;
         this.openRoom = "[Click on a Room to display]";
         this.roomModal = function() {
@@ -15,5 +15,5 @@
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
---
+++ app/scripts/services/Message.js
@@ -0,0 +1,16 @@
+(function() {
+    function Message($firebaseArray) {
+        var ref = firebase.database().ref().child("messages");
+        var messages = $firebaseArray(ref);
+
+        return {
+            getByRoomId: function (roomId) {
+            // Filter the messages by their room ID.
+            }
+        };
+    }
+
+    angular
+        .module('blocChat')
+        .factory('Message', ['$firebaseArray', Message]);
+})();
